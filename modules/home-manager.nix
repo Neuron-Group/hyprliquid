67193cb601814ac520938273da874611edcb9306
Neuron-Group <2062512253@qq.com>
2026-08-22 09:57:13 +0800

    refine the terminal color

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,29 +1,38 @@
-{ self, hyprland }:
-{ config, lib, pkgs, ... }:
-
-let
+{
+  self,
+  hyprland,
+}: {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   inherit (lib) mkAfter mkEnableOption mkIf mkOption types;
   cfg = config.wayland.windowManager.hyprland.hyprliquid;
-  sessionFuzzelConfig = pkgs.writeText "hyprliquid-fuzzel.ini"
-    (builtins.replaceStrings [ "@foot-config@" ] [ cfg.lua.footConfig ] (builtins.readFile ../demo/fuzzel.ini));
+  sessionFuzzelConfig =
+    pkgs.writeText "hyprliquid-fuzzel.ini"
+    (builtins.replaceStrings ["@foot-config@"] [cfg.lua.footConfig] (builtins.readFile ../demo/fuzzel.ini));
   sessionDockStyle = pkgs.writeText "hyprliquid-dock.css" (builtins.readFile ../demo/dock.css);
-  sessionDockControllerText = builtins.replaceStrings
-    [ "@dock-prefix@" "@dock-style@" "@fuzzel-config@" ]
-    [ "hyprliquid-dock" "${sessionDockStyle}" "${sessionFuzzelConfig}" ]
+  sessionDockControllerText =
+    builtins.replaceStrings
+    ["@dock-prefix@" "@dock-style@" "@fuzzel-config@"]
+    ["hyprliquid-dock" "${sessionDockStyle}" "${sessionFuzzelConfig}"]
     (builtins.readFile ../demo/dock-controller.sh);
   sessionDockController = pkgs.writeShellApplication {
     name = "hyprliquid-dock";
-    runtimeInputs = [ pkgs.coreutils pkgs.fuzzel pkgs.nwg-dock-hyprland pkgs.procps ];
+    runtimeInputs = [pkgs.coreutils pkgs.fuzzel pkgs.nwg-dock-hyprland pkgs.procps];
     text = sessionDockControllerText;
   };
-  waybarConfig = pkgs.writeText "hyprliquid-waybar.jsonc"
+  waybarConfig =
+    pkgs.writeText "hyprliquid-waybar.jsonc"
     (builtins.replaceStrings
-      [ "@waybar-launcher@" "@waybar-control-center@" "@fuzzel-config@" ]
-      [ "${config.xdg.configHome}/waybar/images/lambda-launcher.svg" "${config.xdg.configHome}/waybar/images/control-center.svg" "${sessionFuzzelConfig}" ]
+      ["@waybar-launcher@" "@waybar-control-center@" "@fuzzel-config@"]
+      ["${config.xdg.configHome}/waybar/images/lambda-launcher.svg" "${config.xdg.configHome}/waybar/images/control-center.svg" "${sessionFuzzelConfig}"]
       (builtins.readFile ../demo/waybar.jsonc));
-  luaConfig = pkgs.writeText "hyprliquid-home-hyprland.lua"
+  luaConfig =
+    pkgs.writeText "hyprliquid-home-hyprland.lua"
     (builtins.replaceStrings
-      [ "@hyprland@" "@plugin@" "@wallpaper@" "@waybar-config@" "@waybar-style@" "@foot-config@" "@kitty-config@" "@fuzzel-config@" "@dock-controller@" "@panel-command@" ]
+      ["@hyprland@" "@plugin@" "@wallpaper@" "@waybar-config@" "@waybar-style@" "@foot-config@" "@kitty-config@" "@fuzzel-config@" "@dock-controller@" "@panel-command@"]
       [
         "${hyprland.packages.${pkgs.system}.hyprland}/bin/.Hyprland-wrapped"
         "${self.packages.${pkgs.system}.hyprliquid}/lib/libhyprliquid.so"
@@ -91,8 +100,7 @@ let
     workspace = 4, persistent:true
     workspace = 5, persistent:true
   '';
-in
-{
+in {
   options.wayland.windowManager.hyprland.hyprliquid = {
     enable = mkEnableOption "hyprliquid";
 
@@ -160,27 +168,90 @@ in
     };
 
     settings = {
-      enabled = mkOption { type = types.bool; default = true; };
-      background_sharing = mkOption { type = types.bool; default = false; };
-      watch_system_color_scheme = mkOption { type = types.bool; default = true; };
-      effect = mkOption { type = types.either types.int types.str; default = 2; };
-      corner_radius = mkOption { type = types.int; default = 18; };
-      z_radius = mkOption { type = types.int; default = -1; };
-      glass_thickness = mkOption { type = types.float; default = 500.0; };
-      glass_ior = mkOption { type = types.float; default = 1.035; };
-      glass_ior_r = mkOption { type = types.float; default = 1.02; };
-      glass_ior_g = mkOption { type = types.float; default = 1.035; };
-      glass_ior_b = mkOption { type = types.float; default = 1.05; };
-      glass_dispersion = mkOption { type = types.bool; default = true; };
-      vdf_map_mode = mkOption { type = types.int; default = 0; };
-      vdf_map_update_policy = mkOption { type = types.either types.int types.str; default = -2; };
-      vdf_map_debug_mode = mkOption { type = types.int; default = 0; };
-      tint_color = mkOption { type = types.str; default = "rgba(0, 0, 0, 0)"; };
-      brightness = mkOption { type = types.float; default = 1.0; };
-      highlight_style = mkOption { type = types.int; default = 4; };
-      color_scheme = mkOption { type = types.either types.int types.str; default = 3; };
-      aero_reflection_map_path = mkOption { type = types.str; default = ""; };
-      rounding_lua = mkOption { type = types.int; default = -1; };
+      enabled = mkOption {
+        type = types.bool;
+        default = true;
+      };
+      background_sharing = mkOption {
+        type = types.bool;
+        default = false;
+      };
+      watch_system_color_scheme = mkOption {
+        type = types.bool;
+        default = true;
+      };
+      effect = mkOption {
+        type = types.either types.int types.str;
+        default = 2;
+      };
+      corner_radius = mkOption {
+        type = types.int;
+        default = 18;
+      };
+      z_radius = mkOption {
+        type = types.int;
+        default = -1;
+      };
+      glass_thickness = mkOption {
+        type = types.float;
+        default = 500.0;
+      };
+      glass_ior = mkOption {
+        type = types.float;
+        default = 1.035;
+      };
+      glass_ior_r = mkOption {
+        type = types.float;
+        default = 1.02;
+      };
+      glass_ior_g = mkOption {
+        type = types.float;
+        default = 1.035;
+      };
+      glass_ior_b = mkOption {
+        type = types.float;
+        default = 1.05;
+      };
+      glass_dispersion = mkOption {
+        type = types.bool;
+        default = true;
+      };
+      vdf_map_mode = mkOption {
+        type = types.int;
+        default = 0;
+      };
+      vdf_map_update_policy = mkOption {
+        type = types.either types.int types.str;
+        default = -2;
+      };
+      vdf_map_debug_mode = mkOption {
+        type = types.int;
+        default = 0;
+      };
+      tint_color = mkOption {
+        type = types.str;
+        default = "rgba(0, 0, 0, 0)";
+      };
+      brightness = mkOption {
+        type = types.float;
+        default = 1.0;
+      };
+      highlight_style = mkOption {
+        type = types.int;
+        default = 4;
+      };
+      color_scheme = mkOption {
+        type = types.either types.int types.str;
+        default = 3;
+      };
+      aero_reflection_map_path = mkOption {
+        type = types.str;
+        default = "";
+      };
+      rounding_lua = mkOption {
+        type = types.int;
+        default = -1;
+      };
     };
   };
 
@@ -190,73 +261,83 @@ in
       self.packages.${pkgs.system}.hyprliquid
     ];
     programs.waybar.enable = cfg.waybar.enable;
-    wayland.windowManager.hyprland.settings = {
-      input = {
-        kb_layout = "us";
-      };
-      general = {
-        gaps_in = 5;
-        gaps_out = 6;
-        border_size = 2;
-        layout = "dwindle";
-        allow_tearing = false;
-        resize_on_border = true;
-        "col.active_border" = "rgba(8bd5cfff)";
-        "col.inactive_border" = "rgba(6e738d99)";
-      };
-      decoration = {
-        rounding = 18;
-        active_opacity = 1.0;
-        inactive_opacity = 1.0;
-        shadow = {
-          enabled = false;
-          range = 20;
-          render_power = 3;
-          color = "rgba(00000055)";
+    wayland.windowManager.hyprland.settings =
+      {
+        input = {
+          kb_layout = "us";
         };
-        blur = {
+        general = {
+          gaps_in = 5;
+          gaps_out = 6;
+          border_size = 2;
+          layout = "dwindle";
+          allow_tearing = false;
+          resize_on_border = true;
+          "col.active_border" = "rgba(8bd5cfff)";
+          "col.inactive_border" = "rgba(6e738d99)";
+        };
+        decoration = {
+          rounding = 18;
+          active_opacity = 1.0;
+          inactive_opacity = 1.0;
+          shadow = {
+            enabled = false;
+            range = 20;
+            render_power = 3;
+            color = "rgba(00000055)";
+          };
+          blur = {
+            enabled = true;
+            size = 3;
+            passes = 1;
+            new_optimizations = true;
+          };
+        };
+        animations = {
           enabled = true;
-          size = 3;
-          passes = 1;
-          new_optimizations = true;
+          bezier = ["liquid, 0.16, 1, 0.3, 1"];
+          animation = [
+            "windows, 1, 5, liquid"
+            "windowsOut, 1, 5, liquid, popin 80%"
+            "border, 1, 8, liquid"
+            "fade, 1, 5, liquid"
+            "workspaces, 1, 5, liquid, slide"
+          ];
         };
+        misc = {
+          force_default_wallpaper = 0;
+          disable_hyprland_logo = true;
+          disable_splash_rendering = true;
+        };
+        xwayland.enabled = false;
+        plugin.hyprliquid = mkIf (!cfg.lua.enable) cfg.settings;
+        bind = mkAfter (
+          if cfg.hotkeys.enable
+          then defaultHotkeys
+          else []
+        );
+      }
+      // mkIf cfg.session.enable {
+        bind = mkAfter sessionHotkeys;
+        exec-once = mkAfter (lib.optional
+          (cfg.waybar.enable && cfg.waybar.installConfig)
+          "waybar -c ~/.config/waybar/hyprliquid.jsonc -s ~/.config/waybar/hyprliquid.css"
+          ++ [
+            "foot --config ~/.config/foot/hyprliquid.ini --server"
+            "${sessionDockController}/bin/hyprliquid-dock start"
+          ]);
       };
-      animations = {
-        enabled = true;
-        bezier = [ "liquid, 0.16, 1, 0.3, 1" ];
-        animation = [
-          "windows, 1, 5, liquid"
-          "windowsOut, 1, 5, liquid, popin 80%"
-          "border, 1, 8, liquid"
-          "fade, 1, 5, liquid"
-          "workspaces, 1, 5, liquid, slide"
-        ];
-      };
-      misc = {
-        force_default_wallpaper = 0;
-        disable_hyprland_logo = true;
-        disable_splash_rendering = true;
-      };
-      xwayland.enabled = false;
-      plugin.hyprliquid = mkIf (!cfg.lua.enable) cfg.settings;
-      bind = mkAfter (if cfg.hotkeys.enable then defaultHotkeys else [ ]);
-    } // mkIf cfg.session.enable {
-      bind = mkAfter sessionHotkeys;
-      exec-once = mkAfter (lib.optional
-        (cfg.waybar.enable && cfg.waybar.installConfig)
-        "waybar -c ~/.config/waybar/hyprliquid.jsonc -s ~/.config/waybar/hyprliquid.css" ++ [
-        "foot --config ~/.config/foot/hyprliquid.ini --server"
-        "${sessionDockController}/bin/hyprliquid-dock start"
-      ]);
-    };
 
-    home.packages = if cfg.session.enable then [
-      pkgs.foot
-      pkgs.kitty
-      pkgs.fuzzel
-      pkgs.nwg-dock-hyprland
-      sessionDockController
-    ] else [ ];
+    home.packages =
+      if cfg.session.enable
+      then [
+        pkgs.foot
+        pkgs.kitty
+        pkgs.fuzzel
+        pkgs.nwg-dock-hyprland
+        sessionDockController
+      ]
+      else [];
 
     home.file = {
       ".config/hypr/hyprland.lua" = mkIf cfg.lua.enable {
@@ -301,7 +382,7 @@ in
       enable = true;
       type = "fcitx5";
       fcitx5 = {
-        addons = [ pkgs.fcitx5-rime ];
+        addons = [pkgs.fcitx5-rime];
         waylandFrontend = true;
         settings.inputMethod = {
           GroupOrder."0" = "Default";
@@ -322,80 +403,90 @@ in
       };
     };
 
-    wayland.windowManager.hyprland.extraConfig = mkAfter (if cfg.lua.enable then "" else persistentWorkspaceConfig + (if cfg.session.enable then ''
-      windowrule {
-          name = liquid-terminals
-          match:class = ^(foot|kitty)$
-          border_size = 0
-          no_shadow = on
-          opacity = 1.0
-          hyprliquid {
-              effect = liquid_glass
-              rounding_lua = 32
-              tint_color = rgba(10, 88, 205, 0.42)
-          }
-      }
+    wayland.windowManager.hyprland.extraConfig = mkAfter (
+      if cfg.lua.enable
+      then ""
+      else
+        persistentWorkspaceConfig
+        + (
+          if cfg.session.enable
+          then ''
+            windowrule {
+                name = liquid-terminals
+                match:class = ^(foot|kitty)$
+                border_size = 0
+                no_shadow = on
+                opacity = 1.0
+                hyprliquid {
+                    effect = liquid_glass
+                    rounding_lua = 32
+                    tint_color = rgba(10, 88, 205, 0.21)
+                }
+            }
 
-      windowrule {
-          name = acrylic-browser
-          match:class = ^(firefox|zen|chromium)$
-          hyprliquid {
-              effect = acrylic
-              color_scheme = follow_system
-          }
-      }
+            windowrule {
+                name = acrylic-browser
+                match:class = ^(firefox|zen|chromium)$
+                hyprliquid {
+                    effect = acrylic
+                    color_scheme = follow_system
+                }
+            }
 
-      windowrule {
-          name = acrylic-editor
-          match:class = ^(code|codium)$
-          hyprliquid {
-              effect = acrylic
-              color_scheme = follow_system
-          }
-      }
+            windowrule {
+                name = acrylic-editor
+                match:class = ^(code|codium)$
+                hyprliquid {
+                    effect = acrylic
+                    color_scheme = follow_system
+                }
+            }
 
-      layerrule {
-          name = liquid-waybar
-          match:namespace = ^waybar$
-          hyprliquid {
-              effect = liquid_glass
-              corner_radius = 14
-              vdf_map_mode = 1
-              vdf_map_update_policy = always
-              highlight_style = 4
-          }
-      }
+            layerrule {
+                name = liquid-waybar
+                match:namespace = ^waybar$
+                hyprliquid {
+                    effect = liquid_glass
+                    corner_radius = 14
+                    vdf_map_mode = 1
+                    vdf_map_update_policy = always
+                    highlight_style = 4
+                }
+            }
 
-      layerrule {
-          name = liquid-launcher
-          match:namespace = ^hyprliquid-launcher$
-          blur = on
-          hyprliquid {
-              effect = liquid_glass
-              corner_radius = 24
-              z_radius = 24
-              glass_thickness = 700.0
-              glass_dispersion = true
-              brightness = 1.05
-              tint_color = rgba(10, 88, 205, 0.42)
-              highlight_style = 4
-          }
-      }
+            layerrule {
+                name = liquid-launcher
+                match:namespace = ^hyprliquid-launcher$
+                blur = on
+                hyprliquid {
+                    effect = liquid_glass
+                    corner_radius = 24
+                    z_radius = 24
+                    glass_thickness = 700.0
+                    glass_dispersion = true
+                    brightness = 1.05
+                    tint_color = rgba(10, 88, 205, 0.42)
+                    highlight_style = 4
+                }
+            }
 
-      layerrule {
-          name = liquid-dock
-          match:namespace = ^nwg-dock$
-          blur = on
-          hyprliquid {
-              effect = liquid_glass
-              corner_radius = 16
-              glass_thickness = 700.0
-              glass_dispersion = true
-              brightness = 1.05
-              tint_color = rgba(10, 88, 205, 0.42)
-              highlight_style = 4
-          }
-      }
-    '' else ""));
+            layerrule {
+                name = liquid-dock
+                match:namespace = ^nwg-dock$
+                blur = on
+                hyprliquid {
+                    effect = liquid_glass
+                    corner_radius = 16
+                    glass_thickness = 700.0
+                    glass_dispersion = true
+                    brightness = 1.05
+                    tint_color = rgba(10, 88, 205, 0.42)
+                    highlight_style = 4
+                }
+            }
+          ''
+          else ""
+        )
+    );
   };
 }
